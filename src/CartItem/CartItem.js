5a53fa781b8540a898c2fba2fbc153bd858e1737
SebastienMoraz <sebastien@moraz.net>
2024-02-01 09:30:44 +0100

    Fix: make valid cartItem tests

--- src/CartItem/CartItem.js
+++ src/CartItem/CartItem.js
@@ -7,43 +7,70 @@ const InvalidPriceException = require("./InvalidPriceException.js");
 module.exports = class CartItem {
 
     //region private attributes
+    _articleId;
+    _name;
+    _quantity;
+    _price;
     //endregion private attributes
 
     //region public methods
     constructor(articleId, name, quantity, price) {
-	throw new Error();
+        this.CheckValidArticleId(articleId);
+        this.CheckValidQuantity(quantity);
+        this.CheckValidPrice(price);
+        this._articleId = articleId;
+        this._name = name;
+        this._quantity = quantity;
+        this._price = price;
     }
 
     get articleId() {
-        throw new Error();
+        return this._articleId;
     }
 
     get name() {
-        throw new Error();
+        return this._name;
     }
 
     get quantity() {
-        throw new Error();
+        return this._quantity;
     }
 
     set quantity(value) {
-        throw new Error();
+        this.CheckValidQuantity(value);
+        this._quantity = value;
     }
 
     get price() {
-        throw new Error();
+        return this._price;
     }
 
     set price(value) {
-        throw new Error();
+        this.CheckValidPrice(value);
+        this._price = value;
     }
 
     get total() {
-        throw new Error();
+        return this._quantity * this._price;
     }
     //endregion public methods
 
     //region private methods
+    CheckValidArticleId(articleId) {
+        if (articleId < 1) {
+            throw new InvalidArticleIdException();
+        }
+    }
+    CheckValidQuantity(quantity) {
+        if (quantity < 1) {
+            throw new InvalidQuantityException();
+        }
+    }
+    CheckValidPrice(price) {
+        if (price < 10) {
+            throw new InvalidPriceException();
+        }
+    }
     //endregion private methods
 }
 
